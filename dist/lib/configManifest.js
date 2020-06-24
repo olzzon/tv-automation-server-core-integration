6b9d1aa9ceca713ad705ae6cf316cc265e3b8701
Tom Lee <thomasslee97@gmail.com>
2020-06-24 11:04:05 +0100

    dist: Add missing files

---
+++ dist/lib/configManifest.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ConfigManifestEntryType;
+(function (ConfigManifestEntryType) {
+    ConfigManifestEntryType["LABEL"] = "label";
+    ConfigManifestEntryType["LINK"] = "link";
+    ConfigManifestEntryType["STRING"] = "string";
+    ConfigManifestEntryType["MULTILINE_STRING"] = "multiline_string";
+    ConfigManifestEntryType["BOOLEAN"] = "boolean";
+    ConfigManifestEntryType["NUMBER"] = "float";
+    ConfigManifestEntryType["FLOAT"] = "float";
+    ConfigManifestEntryType["INT"] = "int";
+    ConfigManifestEntryType["TABLE"] = "table";
+    ConfigManifestEntryType["OBJECT"] = "object";
+    ConfigManifestEntryType["ENUM"] = "enum"; // @todo: implement
+})(ConfigManifestEntryType = exports.ConfigManifestEntryType || (exports.ConfigManifestEntryType = {}));
+//# sourceMappingURL=configManifest.js.map
